77795d996f5cad4a4ba857fa8b4a34b303f1abec
Angel <angel.villalobos2596@gmail.com>
2021-12-09 16:30:43 -0600

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,20 @@ import React from "react";
 import { Link } from "gatsby";
 
 const SSRpage = ({ serverData }) => {
+
+  const { zones: { MainContentZone }, } = serverData
+
+  console.log(MainContentZone);
+  
   return (
     <>
       <h1>SSR page 1 - HOME </h1>
-      <img
+      <h3>Zones size {MainContentZone.length}</h3>
+      {/* <img
         style={{ width: "300px" }}
         alt="A random dog"
         src={serverData.message}
-      />
+      /> */}
       <p>Welcome to a server side rendered page with a random dog photo</p>
       <p>
         To learn more, head over to our{" "}
@@ -26,21 +32,34 @@ const SSRpage = ({ serverData }) => {
 
 export default SSRpage;
 
+// export async function getServerData() {
+//   try {
+//     const res = await fetch(`https://dog.ceo/api/breeds/image/random`);
+//     if (!res.ok) {
+//       throw new Error(`Response failed`);
+//     }
+//     return {
+//       props: await res.json(),
+//     };
+//   } catch (error) {
+//     return {
+//       headers: {
+//         status: 500,
+//       },
+//       props: {},
+//     };
+//   }
+// }
+
 export async function getServerData() {
-  try {
-    const res = await fetch(`https://dog.ceo/api/breeds/image/random`);
-    if (!res.ok) {
-      throw new Error(`Response failed`);
-    }
-    return {
-      props: await res.json(),
-    };
-  } catch (error) {
-    return {
+  const rest = await fetch(
+    "https://api.aglty.io/800c8044/fetch/en-us/page/111", {
       headers: {
-        status: 500,
+        APIKEY: "defaultlive.42fb0e27ae9606d57c06565f2369f5de856c3982facea37d12308ff2d453851f",
       },
-      props: {},
-    };
+    }
+  )
+  return {
+    props: await rest.json(),
   }
 }
